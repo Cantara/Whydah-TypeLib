3eccbbf008d7df7185b1fe81edef85b3717e52e3
Thor Henning Hetland <totto@totto.org>
2015-11-21 17:01:14 +0100

    more stuff on application credentials and upped v

---
+++ src/main/java/net/whydah/sso/application/helpers/ApplicationCredentialHelper.java
@@ -0,0 +1,18 @@
+package net.whydah.sso.application.helpers;
+
+public class ApplicationCredentialHelper {
+
+
+    public static String getDummyApplicationJson() {
+        return "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?> \n " +
+                "<applicationcredential>\n" +
+                "    <params>\n" +
+                "        <applicationID>XXX</applicationID>\n" +
+                "        <applicationName>DummyApplication</applicationName>\n" +
+                "        <applicationSecret>a very long and super secret passphrase</applicationSecret>\n" +
+                "    </params> \n" +
+                "</applicationcredential>\n";
+    }
+
+
+}
