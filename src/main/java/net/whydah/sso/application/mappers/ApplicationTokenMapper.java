ca9ae62d5c66800e0a103f41460676cabcdb0382
Thor Henning Hetland <totto@totto.org>
2015-11-23 08:05:29 +0100

    Added missing fromXML implementation

--- src/main/java/net/whydah/sso/application/mappers/ApplicationTokenMapper.java
+++ src/main/java/net/whydah/sso/application/mappers/ApplicationTokenMapper.java
@@ -3,8 +3,21 @@ package net.whydah.sso.application.mappers;
 
 import net.whydah.sso.application.helpers.ApplicationTokenXpathHelper;
 import net.whydah.sso.application.types.ApplicationToken;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXParseException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 public class ApplicationTokenMapper {
+    private static final Logger log = LoggerFactory.getLogger(ApplicationTokenMapper.class);
+
 
     public static String toXML(ApplicationToken applicationToken) {
         return "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?> \n " +
@@ -18,6 +31,36 @@ public class ApplicationTokenMapper {
                 "     <Url type=\"application/xml\" method=\"POST\" " +
                 "                template=\"" + applicationToken.getBaseuri() + "user/" + applicationToken.getApplicationTokenId() + "/get_usertoken_by_usertokenid\"/> \n" +
                 " </applicationtoken>\n";
+    }
+
+
+    public static ApplicationToken fromXml(String input) {
+        if (input == null) {
+            return null;
+        }
+        try {
+            DocumentBuilder builder = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+            Document dDoc = builder.parse(input);
+            return extractApplicationToken(dDoc);
+        } catch (SAXParseException pe) {
+            String msg = "fromXml failed due to invalid xml. SAXParseException: " + pe.getMessage();
+            log.debug(msg);
+            throw new IllegalArgumentException(msg);
+        } catch (Exception e) {
+            throw new RuntimeException("Error parsing ApplicationCredential from xml InputStream", e);
+        }
+
+    }
+
+    private static ApplicationToken extractApplicationToken(Document dDoc) throws XPathExpressionException {
+        ApplicationToken applicationToken = new ApplicationToken();
+
+        XPath xPath = XPathFactory.newInstance().newXPath();
+        applicationToken.setApplicationTokenId(xPath.evaluate("//applicationtokenID", dDoc, XPathConstants.STRING).toString());
+        applicationToken.setApplicationID(xPath.evaluate("//applicationid", dDoc, XPathConstants.STRING).toString());
+        applicationToken.setApplicationName(xPath.evaluate("//applicationname", dDoc, XPathConstants.STRING).toString());
+        applicationToken.setExpires(xPath.evaluate("//expires", dDoc, XPathConstants.STRING).toString());
+        return applicationToken;
     }
 
 
