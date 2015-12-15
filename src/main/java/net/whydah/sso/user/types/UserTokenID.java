e3aad3fb77a0b7d316c5e4744db22b16a3d165ba
Thor Henning Hetland <totto@totto.org>
2015-12-15 20:26:14 +0100

    added some extra methods for userTokenId

--- src/main/java/net/whydah/sso/user/types/UserTokenID.java
+++ src/main/java/net/whydah/sso/user/types/UserTokenID.java
@@ -2,14 +2,36 @@ package net.whydah.sso.user.types;
 
 public class UserTokenID {
 
-    public String getId() {
-        return id;
-    }
-
     private final String id;
 
     public UserTokenID(String id) {
         this.id = id;
     }
+
+    public String getUsertokenId() {
+        return id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public boolean isValid() {
+        return id != null;
+    }
+
+    public static UserTokenID fromUserTokenId(String usertokenid) {
+        return new UserTokenID(usertokenid);
+    }
+
+    public static UserTokenID invalidTokenId() {
+        return new UserTokenID(null);
+    }
+
+    @Override
+    public String toString() {
+        return id;
+    }
+
 
 }
