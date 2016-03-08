4cda5c8afa7619398039b3eab218deb14cc226ac
Thor Henning Hetland <totto@totto.org>
2016-03-08 08:01:38 +0100

    some convenience methods

--- src/main/java/net/whydah/sso/extensions/useractivity/helpers/UserActivityHelper.java
+++ src/main/java/net/whydah/sso/extensions/useractivity/helpers/UserActivityHelper.java
@@ -6,7 +6,9 @@ import net.whydah.sso.basehelpers.JsonPathHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class UserActivityHelper {
 
@@ -45,6 +47,53 @@ public class UserActivityHelper {
                     return null;
                 }
                 return mapper.writeValueAsString(applications);
+            }
+        } catch (Exception e) {
+            log.warn("Could not convert getDataElementsFromUserActivityJson Json}");
+        }
+
+        return null;
+    }
+
+    public static String getUserSessionsJsonFromUserActivityJson(String userActivityJson) {
+        try {
+            if (userActivityJson == null) {
+                log.trace("getDataElementsFromUserActivityJson was empty, so returning null.");
+            } else {
+                List<String> applications = JsonPathHelper.findJsonpathList(userActivityJson, "$..userSessions.*");
+                if (applications == null) {
+                    log.debug("jsonpath returned zero hits");
+                    return null;
+                }
+                List<Map> userSessions = new LinkedList<>();
+
+                final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+                Calendar c = new GregorianCalendar();
+
+                int i = 0;
+                while (i < applications.size()) {
+                    Map<String, String> userSession = new HashMap<>();
+                    String activityJson = mapper.writeValueAsString(applications.get(i));
+                    String timestamp = JsonPathHelper.findJsonpathList(userActivityJson, "$..userSessions[" + i + "].startTime").toString();
+                    List<String> data = JsonPathHelper.findJsonpathList(activityJson, "$..data.*");
+                    String activityType = data.get(0);
+                    String applicationid = data.get(1);
+                    String username = data.get(2);
+                    String applicationtokenid = data.get(3);
+                    // data.add("timestamp");
+                    timestamp = timestamp.substring(1, timestamp.length() - 1);
+                    c.setTimeInMillis(Long.parseLong(timestamp));
+                    userSession.put("username", username);
+                    userSession.put("applicationid", applicationid);
+                    userSession.put("activityType", activityType);
+                    userSession.put("timestamp", dateFormat.format(c.getTime()));
+                    userSession.put("applicationtokenid", applicationtokenid);
+
+                    userSessions.add(userSession);
+                    i++;
+                }
+                return mapper.writeValueAsString(userSessions);
             }
         } catch (Exception e) {
             log.warn("Could not convert getDataElementsFromUserActivityJson Json}");
