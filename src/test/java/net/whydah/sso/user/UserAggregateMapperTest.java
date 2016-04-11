fa6ce8b27fef18356e725ff89ceda930cfdfcf95
Thor Henning Hetland <totto@totto.org>
2016-04-11 19:19:59 +0200

    some attempts to re-create an alledged mapping exception on empty lifespan in UserToken XML

--- src/test/java/net/whydah/sso/user/UserAggregateMapperTest.java
+++ src/test/java/net/whydah/sso/user/UserAggregateMapperTest.java
@@ -1,10 +1,14 @@
 package net.whydah.sso.user;
 
+import net.whydah.sso.user.helpers.UserHelper;
 import net.whydah.sso.user.mappers.UserAggregateMapper;
+import net.whydah.sso.user.mappers.UserTokenMapper;
 import net.whydah.sso.user.types.UserAggregate;
+import net.whydah.sso.user.types.UserToken;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class UserAggregateMapperTest {
 
@@ -26,6 +30,10 @@ public class UserAggregateMapperTest {
         String signupJson = "{\"uid\":\"[]\",\"username\":\"totto@totto.org\",\"firstName\":\"Thor Henning\",\"lastName\":\"Hetland\",\"personRef\":\"\",\"email\":\"totto@totto.org\",\"cellPhone\":\"91905054\"}";
         UserAggregate minimalUser = UserAggregateMapper.fromJson(signupJson);
         assertEquals(minimalUser.getUsername(), "totto@totto.org");
-
+        UserToken ut = UserTokenMapper.fromUserTokenXml(UserHelper.getDummyUserToken());
+        assertNotNull(ut);
+        UserToken ut2 = UserTokenMapper.fromUserAggregateJson(UserAggregateMapper.toJson(minimalUser));
+        assertNotNull(ut2);
+        //System.out.println(ut);
     }
 }
