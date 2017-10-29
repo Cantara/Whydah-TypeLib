332b7a5cecabd7bf6978930d1af51c31e637268b
totto <totto@totto.org>
2017-10-29 10:12:51 +0100

    enhanced ApplicationTokenExpire to handle both absolute and relative times as both longs and strings

--- src/main/java/net/whydah/sso/ddd/application/ApplicationTokenExpires.java
+++ src/main/java/net/whydah/sso/ddd/application/ApplicationTokenExpires.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ApplicationTokenExpires implements Serializable {
 
@@ -14,14 +15,49 @@ public class ApplicationTokenExpires implements Serializable {
     public final long ILLEGAL_EXPIRES = -34343434;
     private final static Logger log = LoggerFactory.getLogger(UserTokenLifespan.class);
 
-    public ApplicationTokenExpires(String lifeCycleInMilliseconds) {
-        this(Long.parseLong(lifeCycleInMilliseconds));
+    public ApplicationTokenExpires(String lifeCycleInMilliseconds) throws Exception {
+        if (lifeCycleInMilliseconds.contains("-") && lifeCycleInMilliseconds.contains(" ")) {
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+            Date parsedDate = dateFormat.parse(lifeCycleInMilliseconds);
+            Long tempdate = parsedDate.toInstant().toEpochMilli();
+            Long temp = tempdate - nowTimestamp;
+            if (temp < 0 || temp > 100000000) {
+                log.error("Attempt to create an illegal ApplicationTokenExpires - illegal value:{}", lifeCycleInMilliseconds);
+                this.expiresInMilliseconds = ILLEGAL_EXPIRES;
+            } else {
+                this.expiresInMilliseconds = temp;
+            }
+
+        } else if (lifeCycleInMilliseconds.length() < 10) {
+            Long temp = (Long.parseLong(lifeCycleInMilliseconds));
+
+            if (temp < 0 || temp > 100000000) {
+                log.error("Attempt to create an illegal ApplicationTokenExpires - illegal value:{}", lifeCycleInMilliseconds);
+                this.expiresInMilliseconds = ILLEGAL_EXPIRES;
+            } else {
+                this.expiresInMilliseconds = temp;
+            }
+
+        } else {
+            Date time = new Date(Long.parseLong(lifeCycleInMilliseconds));
+            Long expiresAdjusted = time.toInstant().toEpochMilli() - nowTimestamp;
+            if (expiresAdjusted < 0 || expiresAdjusted > 100000000) {
+                log.error("Attempt to create an illegal ApplicationTokenExpires - illegal value:{}", lifeCycleInMilliseconds);
+                this.expiresInMilliseconds = ILLEGAL_EXPIRES;
+            } else {
+                this.expiresInMilliseconds = expiresAdjusted;
+            }
+        }
+
     }
 
     public ApplicationTokenExpires(long lifeCycleInMilliseconds) {
 
 
-        if (lifeCycleInMilliseconds < 0 || lifeCycleInMilliseconds > 100000000) {
+        // Let us be nice and handle absolute time inputs too i.e.               1509267713753L
+        if (lifeCycleInMilliseconds > 1500000000000L && lifeCycleInMilliseconds < 1520000000000L) {
+            this.expiresInMilliseconds = lifeCycleInMilliseconds - nowTimestamp;
+        } else if (lifeCycleInMilliseconds < 0 || lifeCycleInMilliseconds > 100000000) {
             log.error("Attempt to create an illegal ApplicationTokenExpires - illegal length:{}", lifeCycleInMilliseconds);
             this.expiresInMilliseconds = ILLEGAL_EXPIRES;
         } else {
@@ -34,6 +70,10 @@ public class ApplicationTokenExpires implements Serializable {
         return expiresInMilliseconds;
     }
 
+    public long getValueAsAbsoluteTime() {
+        return expiresInMilliseconds + nowTimestamp;
+    }
+
     public long getSecondValue() {
         return expiresInMilliseconds / 1000;
     }
@@ -41,6 +81,7 @@ public class ApplicationTokenExpires implements Serializable {
     public long getMillisecondValue() {
         return expiresInMilliseconds;
     }
+
 
     public boolean isValid() {
         return expiresInMilliseconds != ILLEGAL_EXPIRES;
--- src/test/java/net/whydah/sso/ddd/application/ApplicationTokenExpiresTest.java
+++ src/test/java/net/whydah/sso/ddd/application/ApplicationTokenExpiresTest.java
@@ -1,23 +1,35 @@
 package net.whydah.sso.ddd.application;
 
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class ApplicationTokenExpiresTest {
+    private static final Logger log = LoggerFactory.getLogger(ApplicationTokenExpiresTest.class);
 
     @Test
-    public void testIllegalApplicationTokenExpires() {
-        assertFalse(new ApplicationTokenExpires(-1).isValid());
-        assertFalse(new ApplicationTokenExpires(432472186).isValid());
+    public void testIllegalApplicationTokenExpires() throws Exception {
+        assertFalse(new ApplicationTokenExpires(-1).isValid());  // Negative delta does not give a meaning
+        assertFalse(new ApplicationTokenExpires("-1").isValid());  // Negative delta does not give a meaning
+        assertFalse(new ApplicationTokenExpires(432472186).isValid());  // Too high interval
+        assertFalse(new ApplicationTokenExpires("432472186").isValid());  // Too high interval
+        assertFalse(new ApplicationTokenExpires(1709343309377L).isValid());  // Too far in the future
+        assertFalse(new ApplicationTokenExpires("1709343309377").isValid());  // Too far in the future
     }
 
     @Test
-    public void testOKApplicationTokenExpires() {
+    public void testOKApplicationTokenExpires() throws Exception {
         assertTrue(new ApplicationTokenExpires(100).isValid());
-        assertTrue(new ApplicationTokenExpires(243543).isValid());
+        assertTrue(new ApplicationTokenExpires(String.valueOf((System.currentTimeMillis()) + 300 * 1000)).isValid());
+        log.debug(String.valueOf((System.currentTimeMillis()) + 5 * 30 * 24 * 60 * 60 * 1000));
+        assertTrue(new ApplicationTokenExpires(1509343309377L).isValid());
+        assertTrue(new ApplicationTokenExpires("1509343309377").isValid());
         assertTrue(new ApplicationTokenExpires(23226566).isValid());
+        assertTrue(new ApplicationTokenExpires("23226566").isValid());
+
     }
 
 }
