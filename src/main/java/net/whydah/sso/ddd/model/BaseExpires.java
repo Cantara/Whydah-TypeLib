0261c8ae932dedb80f5ab6358839b807b2b456d5
totto <totto@totto.org>
2017-11-15 15:16:37 +0100

    and example to add isvalid and imuteability

--- src/main/java/net/whydah/sso/ddd/model/BaseExpires.java
+++ src/main/java/net/whydah/sso/ddd/model/BaseExpires.java
@@ -6,68 +6,68 @@ import java.util.Objects;
 
 public class BaseExpires  extends ValueObject {
 
-    private long nowTimestamp = System.currentTimeMillis();
-    private long expiresInMilliseconds=-34343434;
-    private long max_expiry_range = 100000000; //around 27 hours
+    private final long nowTimestamp = System.currentTimeMillis();
+    private final long expiresInMilliseconds;
+    private final long invalid_value = -232323;
+    private long max_expiry_range = 1000000000; //around 27 hours*10
+    private static final long max_expiry_range_default = 100000000; //around 27 hours
 
     public BaseExpires(String lifeCycleInMilliseconds) {
-    	this(lifeCycleInMilliseconds, 100000000);
+        this(lifeCycleInMilliseconds, max_expiry_range_default);
     }
-    
+
     public BaseExpires(String lifeCycleInMilliseconds, long max_expiry_milisecs) {
-    	if(lifeCycleInMilliseconds==null){
-    		expiresInMilliseconds = 0;
-    		return;
-    	}
-    	this.max_expiry_range = max_expiry_milisecs;
-    	if(lifeCycleInMilliseconds.contains("-") && lifeCycleInMilliseconds.contains(" ")){
-    		
-    		TimeStamp ts = new TimeStamp(lifeCycleInMilliseconds);
-    		long temp = ts.getValue() - nowTimestamp;
-    		assertArgumentRange(temp, 0, max_expiry_range, "Attempt to create an illegal ApplicationTokenExpires - illegal value: " + lifeCycleInMilliseconds);
-    		this.expiresInMilliseconds = temp;
-    		
-    	} else if(lifeCycleInMilliseconds.length() < 10){
-	        
-    		Long temp = (Long.parseLong(lifeCycleInMilliseconds));
-	        assertArgumentRange(temp, 0, max_expiry_range, "Attempt to create an illegal ApplicationTokenExpires - illegal value: " + lifeCycleInMilliseconds);
-	        this.expiresInMilliseconds = temp;
-	        
-    	} else {
-    		 
-    		 Date time = new Date(Long.parseLong(lifeCycleInMilliseconds));
-    		 Long expiresAdjusted = time.toInstant().toEpochMilli() - nowTimestamp;
-    		 assertArgumentRange(expiresAdjusted, 0, max_expiry_range, "Attempt to create an illegal ApplicationTokenExpires - illegal value: " + lifeCycleInMilliseconds + "- expiresAdjusted: " + expiresAdjusted);
-    		 this.expiresInMilliseconds = expiresAdjusted;
-    		 
-    	}
-    
+        if (lifeCycleInMilliseconds == null) {
+            expiresInMilliseconds = invalid_value;
+            return;
+        }
+        this.max_expiry_range = max_expiry_milisecs;
+        if (lifeCycleInMilliseconds.contains("-") && lifeCycleInMilliseconds.contains(" ")) {
+            TimeStamp ts = new TimeStamp(lifeCycleInMilliseconds);
+            long temp = ts.getValue() - nowTimestamp;
+            assertArgumentRange(temp, 0, max_expiry_range, "Attempt to create an illegal BaseExpires - illegal value: " + lifeCycleInMilliseconds);
+            this.expiresInMilliseconds = temp;
+        } else if (lifeCycleInMilliseconds.length() < String.valueOf(max_expiry_range).length()) {
+            Long temp = (Long.parseLong(lifeCycleInMilliseconds));
+            assertArgumentRange(temp, 0, max_expiry_range, "Attempt to create an illegal BaseExpires - illegal value: " + lifeCycleInMilliseconds);
+            this.expiresInMilliseconds = temp;
+        } else {
+            Date time = new Date(Long.parseLong(lifeCycleInMilliseconds));
+            Long expiresAdjusted = time.toInstant().toEpochMilli() - nowTimestamp;
+            assertArgumentRange(expiresAdjusted, 0, max_expiry_range, "Attempt to create an illegal BaseExpires - illegal value: " + lifeCycleInMilliseconds + "- expiresAdjusted: " + expiresAdjusted);
+            this.expiresInMilliseconds = expiresAdjusted;
+        }
+
 
     }
-    
+
     public BaseExpires(long lifeCycleInMilliseconds) {
-    	this(lifeCycleInMilliseconds, 100000000);
+        this(lifeCycleInMilliseconds, max_expiry_range_default);
     }
 
     public BaseExpires(long lifeCycleInMilliseconds, long max_expiry_milisecs) {
-    	this.max_expiry_range = max_expiry_milisecs;
-    	
+        this.max_expiry_range = max_expiry_milisecs;
+
         if (lifeCycleInMilliseconds > 1500000000000L && lifeCycleInMilliseconds < 1520000000000L) {
-            assertStateTrue(lifeCycleInMilliseconds - nowTimestamp >= 0, String.format("Attempt to create an illegal ApplicationTokenExpires - time in the past:{} time now:{}", lifeCycleInMilliseconds, nowTimestamp));  
+            assertStateTrue(lifeCycleInMilliseconds - nowTimestamp >= 0, String.format("Attempt to create an illegal BaseExpires - time in the past:%s time now:%s", lifeCycleInMilliseconds, nowTimestamp));
             this.expiresInMilliseconds = lifeCycleInMilliseconds - nowTimestamp;
         } else {
-        	assertArgumentRange(lifeCycleInMilliseconds, 0, max_expiry_range, "Attempt to create an illegal ApplicationTokenExpires - illegal value: " + lifeCycleInMilliseconds);
+            assertArgumentRange(lifeCycleInMilliseconds, 0, max_expiry_range, "Attempt to create an illegal BaseExpires - illegal value: " + lifeCycleInMilliseconds);
             this.expiresInMilliseconds = lifeCycleInMilliseconds;
         }
-    	
+
     }
 
     public long getTimeoutInterval(){
-    	return expiresInMilliseconds;
+        return expiresInMilliseconds;
     }
 
     public long getValue() {
         return expiresInMilliseconds + nowTimestamp;
+    }
+
+    public boolean isvalid() {
+        return expiresInMilliseconds != invalid_value;
     }
 
     public long getValueAsAbsoluteTimeInMilliseconds() {
@@ -77,9 +77,9 @@ public class BaseExpires  extends ValueObject {
     public long getValueAsRelativeTimeInMilliseconds() {
         return expiresInMilliseconds + nowTimestamp;
     }
-    
+
     public long getSecondValue() {
-        return expiresInMilliseconds + nowTimestamp / 1000;
+        return (expiresInMilliseconds + nowTimestamp) / 1000;
     }
 
     public long getMillisecondValue() {
@@ -90,14 +90,18 @@ public class BaseExpires  extends ValueObject {
         return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(nowTimestamp + expiresInMilliseconds);
     }
 
+    public static boolean isvalid(String s) {
+        return new BaseExpires(s).isvalid();
+    }
+
     @Override
     public int hashCode() {
-    	return Objects.hash(getTimeoutInterval());
+        return Objects.hash(getTimeoutInterval());
     }
-    
+
     @Override
     public boolean equals(Object o) {
-    	if (o == this) return true;
+        if (o == this) return true;
         if (!(o instanceof BaseExpires)) {
             return false;
         }
