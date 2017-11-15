3cd40349e93ba7e1a740a55a788e57aaf0f1cf2d
totto <totto@totto.org>
2017-11-15 17:52:17 +0100

    test to show some validator issues

---
+++ src/test/java/net/whydah/sso/ddd/model/ApplicationUrlTest.java
@@ -0,0 +1,33 @@
+package net.whydah.sso.ddd.model;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class ApplicationUrlTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ApplicationUrlTest.class);
+
+    @Test
+    public void testIllegalApplicationUrl() throws Exception {
+        assertFalse(ApplicationUrl.isValid("httpa://whydahdev.cantara.no"));
+        assertFalse(ApplicationUrl.isValid("-1"));  // Negative delta does not give a meaning
+        assertFalse(ApplicationUrl.isValid(String.valueOf((System.currentTimeMillis()) - 300 * 1000)));  // time in the past
+        assertFalse(ApplicationUrl.isValid("1432472186"));  // Too high interval
+        assertFalse(ApplicationUrl.isValid("1709343309377"));  // Too far in the future
+    }
+
+    @Test
+    public void testOKApplicationUrl() throws Exception {
+        assertTrue(ApplicationUrl.isValid("http://whydahdev.cantara.no"));
+        assertTrue(ApplicationUrl.isValid("https://whydahdev.cantara.no"));
+        assertTrue(ApplicationUrl.isValid("https://myUrl.com"));
+        assertTrue(ApplicationUrl.isValid("https://myUrl.com/"));
+        assertTrue(ApplicationUrl.isValid("https://myUrl.com/testpage"));
+
+    }
+
+}
