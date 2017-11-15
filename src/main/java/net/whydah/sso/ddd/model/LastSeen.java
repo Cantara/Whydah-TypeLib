24dbc9558ad709394af5737bb7ede07fde6e865e
totto <totto@totto.org>
2017-11-15 20:42:30 +0100

    some work on lastSeen

--- src/main/java/net/whydah/sso/ddd/model/LastSeen.java
+++ src/main/java/net/whydah/sso/ddd/model/LastSeen.java
@@ -1,18 +1,18 @@
 package net.whydah.sso.ddd.model;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class LastSeen extends TimeStamp {
+
+
+    public static final String NOT_SEEN = "Not seen";
 
 	public LastSeen() {
 		super(System.currentTimeMillis());
 	}
 	
 	public LastSeen(String lastseen) {
-		super(lastseen);
-	}
+        super(lastseen);
+    }
 	
 	public LastSeen(long lastseen){
 		super(lastseen);
@@ -20,24 +20,29 @@ public class LastSeen extends TimeStamp {
 	
 	@Override
 	protected void validateInput(String ts, long time) {
-		assertStateTrue(time <= System.currentTimeMillis(), "Illegal input: " + ts);
-	}
+        assertStateTrue(time <= System.currentTimeMillis(), "Illegal input, lastSeen can't be in the future: " + ts);
+    }
 
 
 	public static boolean isValid(String lastSeen) {
 		try {
-			new LastSeen(lastSeen);
-			return true;
-		} catch (Exception e) {
+//            if (NOT_SEEN.equalsIgnoreCase(lastSeen)){
+//                return true;
+//            }
+            LastSeen lastSeen1 = new LastSeen(lastSeen);
+            lastSeen1.validateInput(lastSeen, Long.parseLong(lastSeen));
+            return true;
+        } catch (Exception e) {
 		}
 		return false;
 	}
 
 	public static boolean isValid(long lastSeen) {
 		try {
-			new LastSeen(lastSeen);
-			return true;
-		} catch (Exception e) {
+            LastSeen lastSeen1 = new LastSeen(lastSeen);
+            lastSeen1.validateInput(Long.toString(lastSeen), lastSeen);
+            return true;
+        } catch (Exception e) {
 		}
 		return false;
 	}
