a9203a97338e8983307a604019a36a8439be1448
totto <totto@totto.org>
2017-11-15 18:07:00 +0100

    test to show some validator issues

--- src/main/java/net/whydah/sso/application/helpers/ApplicationCredentialHelper.java
+++ src/main/java/net/whydah/sso/application/helpers/ApplicationCredentialHelper.java
@@ -10,7 +10,7 @@ public class ApplicationCredentialHelper {
                         "        <applicationID>XXX</applicationID>\n" +
                         "        <applicationName>DummyApplication</applicationName>\n" +
                         "        <applicationSecret>a very long and super secret passphrase</applicationSecret>\n" +
-                        "        <applicationurl> http(s)://myUrl </applicationurl>\n" +
+                        "        <applicationurl> https://myUrl.com</applicationurl>\n" +
                         "        <minimumsecuritylevel>2</minimumsecuritylevel>\n" +
                         "     </params> \n" +
                         "</applicationcredential>\n";
