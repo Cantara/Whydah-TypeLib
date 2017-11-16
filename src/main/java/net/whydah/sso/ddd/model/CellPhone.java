c4ee8bc953f67842cf75ea31fb524097df413e63
totto <totto@totto.org>
2017-11-16 18:20:47 +0100

    added tests for CellPhone

--- src/main/java/net/whydah/sso/ddd/model/CellPhone.java
+++ src/main/java/net/whydah/sso/ddd/model/CellPhone.java
@@ -5,13 +5,13 @@ import net.whydah.sso.basehelpers.Validator;
 public class CellPhone extends AbstractName {
 
 	public CellPhone(String input) {
-		super(input);
+		super(input.replaceAll(" ", ""));
 	}
 	
 	@Override
 	protected void validateInput(String input) {
 		super.validateInput(input);
-		assertArgumentWithAPattern(input, Validator.DEFAULT_PHONE_NUMBER_PATTERN, "Invalid phone number " + input);
+		assertArgumentWithAPattern(input, Validator.DEFAULT_CELLPHONE_NUMBER_PATTERN, "Invalid cellphone number " + input);
 	}
 	
 	public static boolean isValid(String input) {
