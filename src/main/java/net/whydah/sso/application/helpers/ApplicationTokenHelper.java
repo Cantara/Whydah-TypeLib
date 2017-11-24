27f09103eb8a244676029884a3dc28adfc973b84
totto <totto@totto.org>
2017-11-24 09:30:12 +0100

    valid expires value in dummy token

--- src/main/java/net/whydah/sso/application/helpers/ApplicationTokenHelper.java
+++ src/main/java/net/whydah/sso/application/helpers/ApplicationTokenHelper.java
@@ -10,7 +10,7 @@ public class ApplicationTokenHelper {
                 "         <applicationtokenID>1cca06386f52f91d9610aa1dbd95b9a9</applicationtokenID>\n" +
                 "         <applicationid>2210</applicationid>\n" +
                 "         <applicationname>Whydah-UserIdentityBackend</applicationname>\n" +
-                "         <expires>1448277365860</expires>\n" +
+                "         <expires>" + System.currentTimeMillis() + 50000 + "</expires>\n" +
                 "     </params> \n" +
                 "     <Url type=\"application/xml\" method=\"POST\"                 template=\"https://whydahdev.cantara.no/tokenservice/user/1cca06386f52f91d9610aa1dbd95b9a9/get_usertoken_by_usertokenid\"/> \n" +
                 "   </applicationtoken>";
