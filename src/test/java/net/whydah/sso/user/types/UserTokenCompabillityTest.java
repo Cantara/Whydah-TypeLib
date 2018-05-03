124be9d70bc232b461a9ed569e66687b8cd0d456
totto <totto@totto.org>
2018-05-03 12:23:36 +0200

    added support for claspath loading

--- src/test/java/net/whydah/sso/user/types/UserTokenCompabillityTest.java
+++ src/test/java/net/whydah/sso/user/types/UserTokenCompabillityTest.java
@@ -2,6 +2,8 @@ package net.whydah.sso.user.types;
 
 import net.whydah.sso.user.mappers.UserTokenMapper;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -9,6 +11,9 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 public class UserTokenCompabillityTest {
+
+
+    private static final Logger log = LoggerFactory.getLogger(UserTokenCompabillityTest.class);
 
     @Test
     public void testSerializeUserToken() throws Exception {
@@ -72,10 +77,27 @@ public class UserTokenCompabillityTest {
 
     @Test
     public void testDeSerializeUserToken() throws Exception {
-        FileInputStream streamIn = new FileInputStream("./usertoken_old.ser");
-        ObjectInputStream objectinputstream = new ObjectInputStream(streamIn);
+        ObjectInputStream objectinputstream = new ObjectInputStream(loadByName("./usertoken_old.ser"));
         UserToken oldUserToken = (UserToken) objectinputstream.readObject();
         System.out.println(oldUserToken);
 
     }
+
+    public static java.io.InputStream loadByName(String name) {
+        try {
+            java.io.File f = new java.io.File(name);
+            if (f.isFile()) {
+                log.info("Loading {} from Filesystem", name);
+                return new FileInputStream(f);
+            } else {
+
+                log.info("Loading {} from Classpath", name);
+                return UserTokenCompabillityTest.class.getClassLoader().getResourceAsStream(name);
+            }
+        } catch (Exception e) {
+            log.error("Unable to access file:{}, exception: {} ", name, e);
+        }
+        return null;
+    }
+
 }
