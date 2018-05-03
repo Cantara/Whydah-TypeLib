a5e6b9109d5273bf201f53db7ed6035d937d5567
totto <totto@totto.org>
2018-05-03 12:06:57 +0200

    added serialization/deserialization testcase to detect incompatible typechange of UserToken

---
+++ src/test/java/net/whydah/sso/user/types/UserTokenCompabillityTest.java
@@ -0,0 +1,81 @@
+package net.whydah.sso.user.types;
+
+import net.whydah.sso.user.mappers.UserTokenMapper;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class UserTokenCompabillityTest {
+
+    @Test
+    public void testSerializeUserToken() throws Exception {
+        String identityXML = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                "<whydahuser>\n" +
+                "    <identity>\n" +
+                "        <username>admin</username>\n" +
+                "        <cellPhone>+1555406789</cellPhone>\n" +
+                "        <email>useradmin@getwhydah.com</email>\n" +
+                "        <firstname>User</firstname>\n" +
+                "        <lastname>Admin</lastname>\n" +
+                "        <personref>220</personref>\n" +
+                "        <UID>useradmin</UID>\n" +
+                "    </identity>\n" +
+                "    <applications>\n" +
+                "        <application>\n" +
+                "            <appId>1923</appId>\n" +
+                "            <applicationName>UserAdminWebApplication</applicationName>\n" +
+                "            <orgName>Support</orgName>\n" +
+                "            <roleName>WhydahUserAdmin</roleName>\n" +
+                "            <roleValue>1</roleValue>\n" +
+                "        </application>\n" +
+                "        <application>\n" +
+                "            <appId>1923</appId>\n" +
+                "            <applicationName>UserAdminWebApplication</applicationName>\n" +
+                "            <orgName>Support</orgName>\n" +
+                "            <roleName>TEST</roleName>\n" +
+                "            <roleValue>13</roleValue>\n" +
+                "        </application>\n" +
+                "        <application>\n" +
+                "            <appId>1923</appId>\n" +
+                "            <applicationName>UserAdminWebApplication</applicationName>\n" +
+                "            <orgName>ACS</orgName>\n" +
+                "            <roleName>TULL</roleName>\n" +
+                "            <roleValue>1</roleValue>\n" +
+                "        </application>\n" +
+                "        <application>\n" +
+                "            <appId>1923</appId>\n" +
+                "            <applicationName>UserAdminWebApplication</applicationName>\n" +
+                "            <orgName>Support</orgName>\n" +
+                "            <roleName>WhydahUserAdmin</roleName>\n" +
+                "            <roleValue>1</roleValue>\n" +
+                "        </application>\n" +
+                "        <application>\n" +
+                "            <appId>1923</appId>\n" +
+                "            <applicationName>UserAdminWebApplication</applicationName>\n" +
+                "            <orgName>Support</orgName>\n" +
+                "            <roleName>UserAdmin</roleName>\n" +
+                "            <roleValue>100</roleValue>\n" +
+                "        </application>\n" +
+                "    </applications>\n" +
+                "</whydahuser>\n";
+
+        UserToken userToken = UserTokenMapper.fromUserAggregateXml(identityXML);
+
+        FileOutputStream fout = new FileOutputStream("usertoken_new.ser");
+        ObjectOutputStream oos = new ObjectOutputStream(fout);
+        oos.writeObject(userToken);
+
+    }
+
+    @Test
+    public void testDeSerializeUserToken() throws Exception {
+        FileInputStream streamIn = new FileInputStream("./usertoken_old.ser");
+        ObjectInputStream objectinputstream = new ObjectInputStream(streamIn);
+        UserToken oldUserToken = (UserToken) objectinputstream.readObject();
+        System.out.println(oldUserToken);
+
+    }
+}
