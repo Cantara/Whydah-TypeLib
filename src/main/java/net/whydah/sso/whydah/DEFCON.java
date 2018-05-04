fc4f492bafc31f667a3df1583d56a3bed3b717a5
totto <totto@totto.org>
2018-05-04 09:02:37 +0200

    maybe?

--- src/main/java/net/whydah/sso/whydah/DEFCON.java
+++ src/main/java/net/whydah/sso/whydah/DEFCON.java
@@ -4,6 +4,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public enum DEFCON {
     @JsonProperty("DEFCON")
-    DEFCON1, DEFCON2, DEFCON3, DEFCON4, DEFCON5
+    DEFCON5, DEFCON1, DEFCON2, DEFCON3, DEFCON4
 }
 
